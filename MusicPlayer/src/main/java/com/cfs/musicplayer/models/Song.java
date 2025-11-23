2fcf42d6959c5b4e5090e154dfe60b20a73f729b
Aman Gautam <amangautam2128@gmail.com>
2025-11-23 17:17:27 +0530

    started Music Player App

---
+++ MusicPlayer/src/main/java/com/cfs/musicplayer/models/Song.java
@@ -0,0 +1,23 @@
+package com.cfs.musicplayer.models;
+
+public class Song {
+    String title;
+    String artist;
+    String filePath;
+
+    public Song(String title, String artist, String filePath) {
+        this.title = title;
+        this.artist = artist;
+        this.filePath = filePath;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getArtist() {
+        return artist;
+    }
+}
