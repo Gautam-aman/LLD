f536c07a4af16a87964830120dbe3db2a7107b3d
Aman Gautam <amangautam2128@gmail.com>
2025-11-27 16:07:38 +0530

    Added validstion logicn:

--- PaymentGateway/src/main/java/com/cfs/paymentgateway/Main.java
+++ PaymentGateway/src/main/java/com/cfs/paymentgateway/Main.java
@@ -23,10 +23,88 @@ class GooglePayBankingSystem implements BankingSystem{
         System.out.println("GooglePayBankingSystem processPayment");
         Random rand = new Random();
         int r = rand.nextInt(10);
-        return r > 5;
+        return r > 2;
     }
 }
 
+ abstract class PaymentGateway {
+    public BankingSystem bankingSystem;
+    public PaymentGateway(BankingSystem bankingSystem) {
+        this.bankingSystem = bankingSystem;
+    }
+
+    public boolean processPayment(PaymentRequest paymentRequest) {
+        if (!validate(paymentRequest)){
+            System.out.println("Validation Failed");
+            return false;
+        }
+        if (!initiatePayment(paymentRequest)){
+            System.out.println("Initiation Failed");
+            return false;
+        }
+        if (!confirmPayment(paymentRequest)){
+            System.out.println("Confirmation Failed");
+            return false;
+        }
+        return true;
+    }
+    public abstract boolean  initiatePayment(PaymentRequest paymentRequest);
+    public abstract boolean confirmPayment(PaymentRequest paymentRequest) ;
+    public abstract boolean validate(PaymentRequest paymentRequest) ;
+}
+
+class PaytmGateway extends PaymentGateway {
+     public PaytmGateway() {
+       super(new PaytmBankingSystem());
+    }
+
+
+    @Override
+    public boolean initiatePayment(PaymentRequest paymentRequest) {
+        System.out.println("PaytmGateway initiatePayment");
+        return bankingSystem.processPayment();
+    }
+
+    @Override
+    public boolean confirmPayment(PaymentRequest paymentRequest) {
+        System.out.println("Payment Successfull");
+        return true;
+    }
+
+    @Override
+    public boolean validate(PaymentRequest paymentRequest) {
+        System.out.println("Validating Payment");
+        double amount = paymentRequest.getAmount();
+        if(amount < 0 || !paymentRequest.getCurrency().equals("INR")) {
+            return false;
+        }
+        return true;
+    }
+}
+
+class GooglePayGateway extends PaymentGateway {
+    public GooglePayGateway() {
+        super(new PaytmBankingSystem());
+    }
+    @Override
+    public boolean initiatePayment(PaymentRequest paymentRequest) {
+        System.out.println("GooglePayGateway initiatePayment");
+        return bankingSystem.processPayment();
+    }
+    @Override
+    public boolean confirmPayment(PaymentRequest paymentRequest) {
+        System.out.println("Confirming Payment");
+        return true;
+    }
+    @Override
+    public boolean validate(PaymentRequest paymentRequest) {
+        double amount = paymentRequest.getAmount();
+        if(amount < 0 || !paymentRequest.getCurrency().equals("INR")) {
+            return false;
+        }
+        return true;
+    }
+}
 
 
 
--- PaymentGateway/src/main/java/com/cfs/paymentgateway/PaymentRequest.java
+++ PaymentGateway/src/main/java/com/cfs/paymentgateway/PaymentRequest.java
@@ -12,7 +12,37 @@ public class PaymentRequest {
         this.Reciever = reciever;
         this.amount = amount;
         this.currency = currency;
-
     }
 
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getReciever() {
+        return Reciever;
+    }
+
+    public void setReciever(String reciever) {
+        Reciever = reciever;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
 }
