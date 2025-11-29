5007f10b098b3981f179287fec10ac6aa5c9e360
Aman Gautam <amangautam2128@gmail.com>
2025-11-29 17:31:29 +0530

    Added Discount Coupon Engine

---
+++ Discount Coupon Engine/src/main/java/com/cfs/discountcouponengine/DiscountCouponEngineApplication.java
@@ -0,0 +1,13 @@
+package com.cfs.discountcouponengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscountCouponEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscountCouponEngineApplication.class, args);
+    }
+
+}
---
+++ Discount Coupon Engine/src/test/java/com/cfs/discountcouponengine/DiscountCouponEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cfs.discountcouponengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscountCouponEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
