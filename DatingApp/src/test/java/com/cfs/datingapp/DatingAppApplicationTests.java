a349ae8350c6d78d24717e1a23233eb39948ddbc
Aman Gautam <amangautam2128@gmail.com>
2025-12-07 17:36:13 +0530

    Started lld of dating app

---
+++ DatingApp/src/test/java/com/cfs/datingapp/DatingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cfs.datingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
