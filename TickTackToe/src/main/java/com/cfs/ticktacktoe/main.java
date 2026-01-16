96d93e99bc1d792f043b243db5ee8679a661bceb
Aman Gautam <amangautam2128@gmail.com>
2026-01-16 22:34:59 +0530

    win condition

--- TickTackToe/src/main/java/com/cfs/ticktacktoe/main.java
+++ TickTackToe/src/main/java/com/cfs/ticktacktoe/main.java
@@ -107,8 +107,81 @@ class GamePlayer{
     public Symbol getSymbol() {
         return symbol;
     }
-
 }
+
+interface GameRules{
+    public boolean isValidMove(int row, int col , Board board);
+    public boolean checkWinCondition(Symbol symbol, Board board);
+    public boolean checkDrawCondition(Board board);
+}
+
+ class StandardRules implements GameRules{
+
+     @Override
+     public boolean isValidMove(int row, int col, Board board) {
+         return board.isCellEmpty(row, col);
+     }
+
+     @Override
+     public boolean checkWinCondition(Symbol symbol, Board board) {
+         int size = board.getSize();
+         for(int i = 0 ; i < size ; i++){
+             boolean win = false;
+             for(int j = 0 ; j < size ; j++){
+                 if (!board.getCell(i, j).equals(symbol)){
+                     win = false;
+                     break;
+                 }
+             }
+             if(win){
+                 return true;
+             }
+         }
+
+         // check col
+         for(int i = 0 ; i < size ; i++){
+             boolean win = false;
+             for(int j = 0 ; j < size ; j++){
+                 if(!board.getCell(i, j).equals(symbol)){
+                     win = false;
+                 }
+             }
+             if(win){
+                 return true;
+             }
+         }
+
+         // check diag
+         boolean win = true;
+         for (int i = 0 ; i < size ; i++){
+             if (!board.getCell(i, i).equals(symbol)){
+                 win = false;
+                 break;
+             }
+         }
+         if(win){
+             return true;
+         }
+
+         // anti diag
+         for(int i = 0 ; i < size ; i++){
+             if (!board.getCell(i, size-1-i).equals(symbol)){
+                 win = false;
+             }
+         }
+         if(win){
+             return true;
+         }
+
+         return false;
+
+     }
+
+     @Override
+     public boolean checkDrawCondition(Board board) {
+         return false;
+     }
+ }
 
 public class main {
 
